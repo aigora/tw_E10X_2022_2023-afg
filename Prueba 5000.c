0c9932984a181358e3ac545f2bd40139f8d3c9df
arnoldcampoverde <arnold.campoverde@alumnos.upm.es>
2023-05-24 12:46:54 +0200

    correciones

--- Prueba 5000.c
+++ Prueba 5000.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
-#include <string.h>
-
-#define MAX_LINE_LENGTH 256
-#define MAX_LINES 30
 
 int main() {
-    FILE *fp;
-    char line[MAX_LINE_LENGTH];
-    char lines[MAX_LINES][MAX_LINE_LENGTH];
-    int i, num_lines = 0;
+    float hidraulica[24];
+    float turbinacion[24];
+    float nuclear[24];
+    float carbon[24];
+    float motores[24];
+    float turbinagas[24];
+    float turbinavapor[24];
+    float ciclocombinado[24];
+    float hidroeolica[24];
+    float eolica[24];
+    float solarfotov[24];
+    float solartermica[24];
+    float renovables[24];
+    float cogeneracion[24];
+    float residuosNR[24];
+    float residuosR[24];
+    float generacionT[24];
+    int i;
 
-    char filename[] = "C:/Users/arnol/Downloads/generacion_por_tecnologias_21_22_puntos_simplificado.csv";
-
-    fp = fopen(filename, "r");
-    if (fp == NULL) {
-        printf("Error al abrir el archivo\n");
+    FILE *pf;
+    pf = fopen("c:/Users/arnol/Downloads/generacion_por_tecnologias_21_22_puntos_simplificado.csv", "r");
+    if (pf == NULL) {
+        printf("No se ha podido abrir el fichero.\n");
         return 1;
     }
 
-    fgets(line, sizeof(line), fp); // Leer la primera línea y descartarla
-
-    while (fgets(line, sizeof(line), fp) != NULL && num_lines < MAX_LINES) {
-        int length = strlen(line);
-        if (line[length - 1] == '\n') {
-            line[length - 1] = '\0'; // Reemplazar el carácter de nueva línea por el carácter nulo
-        }
-        strcpy(lines[num_lines], line);
-        num_lines++;
+    // Saltar las líneas de encabezado y las primeras 5 líneas
+    char line[500];
+    for (i = 0; i < 5; i++) {
+        fgets(line, sizeof(line), pf);
     }
 
-    fclose(fp);
-
-    for (i = 0; i < num_lines; i++) {
-        printf("%s\n", lines[i]);
+    // Leer los valores del archivo y guardarlos en los vectores correspondientes
+    for (i = 0; i < 24; i++) {
+        fscanf(pf, "%*[^,] ,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f",
+            &hidraulica[i], &turbinacion[i], &nuclear[i], &carbon[i], &motores[i], &turbinagas[i],
+            &turbinavapor[i], &ciclocombinado[i], &hidroeolica[i], &eolica[i], &solarfotov[i],
+            &solartermica[i], &renovables[i], &cogeneracion[i], &residuosNR[i], &residuosR[i], &generacionT[i]);
     }
+
+    fclose(pf);
+
+    for (i = 0; i < 12; i++) {
+        printf("%f, ", hidraulica[i]);
+    }
+
+    // Los datos han sido leídos y almacenados en los vectores correspondientes
+    // Puedes hacer uso de estos vectores según tus necesidades
 
     return 0;
 }
-
 
