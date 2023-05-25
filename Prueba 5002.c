162d0a724f4b512e2d87e4aaaea699a93ef9c018
arnoldcampoverde <arnold.campoverde@alumnos.upm.es>
2023-05-25 11:15:04 +0200

    Update Prueba 5002.c

--- Prueba 5002.c
+++ Prueba 5002.c
@@ -2,7 +2,8 @@
 
 int main() {
 	
-    float hidraulica[24], turbinacion[24], nuclaer[24], carbon[24];
+    float hidraulica[24], turbinacion[24], nuclaer[24], carbon[24], motor[24], turbinag[24], turbinav[24], ciclo[24], hidroelectrica[24];
+    float solarfoto[24];
     char fichero1[] = "c:/Users/arnol/Downloads/generacion_por_tecnologias_21_22_puntos_simplificado.csv";
     int i;
        
@@ -58,32 +59,142 @@ int main() {
             fscanf(pf, "%f,", &carbon[i]);
         }
     }
+    
+    // Motor diesel
+    for (i = 0; i < 9; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &motor[i]);
+        }
+    }
+    
+    
+    // Turbina gas
+    for (i = 0; i < 10; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &turbinag[i]);
+        }
+    }
+
+
+    // Turbina vapor
+    for (i = 0; i < 11; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &turbinav[i]);
+        }
+    }
+    
+    // Ciclo combinado
+    for (i = 0; i < 12; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &ciclo[i]);
+        }
+    }
+    
+    // Hidroelectrica
+    for (i = 0; i < 13; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &hidroelectrica[i]);
+        }
+    }
+    
+    // Solarfotovoltaica
+    for (i = 0; i < 14; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &solarfoto[i]);
+        }
+    }
 
     fclose(pf);
+    
+    
+    
+    // Imprimir vectores
 
-    printf("Hidraulica:\n");
+    printf("Hidraulica: ");
     for (i = 0; i < 24; i++) {
         printf("%f ", hidraulica[i]);
     }
     printf("\n\n");
     
-    printf("Turbinacion:\n");
+    printf("Turbinacion: ");
     for (i = 0; i < 24; i++) {
         printf("%f ", turbinacion[i]);
     }
     printf("\n\n");
     
-    printf("Nuclaer:\n");
+    printf("Nuclaer: ");
     for (i = 0; i < 24; i++) {
         printf("%f ", nuclaer[i]);
     }
     printf("\n\n");
     
-    printf("Carbon:\n");
+    printf("Carbon: ");
     for (i = 0; i < 24; i++) {
         printf("%f ", carbon[i]);
     }
     printf("\n\n");
+    
+    printf("Motor diesel: ");
+    for (i = 0; i < 24; i++) {
+        printf("%f ", motor[i]);
+    }
+    printf("\n\n");
+    
+    printf("Turbina gas: ");
+    for (i = 0; i < 24; i++) {
+        printf("%f ", turbinag[i]);
+    }
+    printf("\n\n");
+    
+    printf("Turbina vapor: ");
+    for (i = 0; i < 24; i++) {
+        printf("%f ", turbinav[i]);
+    }
+    printf("\n\n");
+    
+    printf("Ciclo combinado: ");
+    for (i = 0; i < 24; i++) {
+        printf("%f ", ciclo[i]);
+    }
+    printf("\n\n");
+    
+    printf("Hidroelctrica: ");
+    for (i = 0; i < 24; i++) {
+        printf("%f ", hidroelectrica[i]);
+    }
+    printf("\n\n");
+    
+    printf("Solar fotovoltaica: ");
+    for (i = 0; i < 24; i++) {
+        printf("%f ", solarfoto[i]);
+    }
+    printf("\n\n");
+    
+    
 
     return 0;
 }
