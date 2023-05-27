2ff2617b054e880217f1e2aafcfe74515d8bfe59
FernandaBasagoiti <102177781+FernandaBasagoiti@users.noreply.github.com>
2023-05-27 19:03:50 +0200

    Add files via upload

---
+++ Búsqueda de datos (definitivo).c
@@ -0,0 +1,331 @@
+//Búsqueda de elementos.
+
+#include <stdio.h>
+
+void buscar_datos (float v [24]);
+
+int main () {
+	
+    float hidraulica[24], turbinacion[24], nuclear[24], carbon[24], motor[24], turbinag[24], turbinav[24], ciclo[24], hidroelectrica[24];
+    float eolica[24], solarfoto[24], solarter[24], renovables[24], cogeneracion[24], residuosnor[24], residuosr[24], generaciontotal[24];
+    char fichero1[] = "c:/Users/Fernanda/OneDrive/Desktop/Trabajo/generacion_por_tecnologias_21_22_puntos_simplificado.csv";
+    int i, dato;
+       
+    FILE *pf;
+    pf = fopen(fichero1, "r");
+    if (pf == NULL) {
+        printf("No se ha podido abrir el fichero.\n");
+    }
+    
+    // Datos en vectores
+
+    // 1 - Hidraulica
+    for (i = 0; i < 5; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &hidraulica[i]);
+        }
+    }
+     
+    // 2 - Turbinacion
+    for (i = 0; i < 6; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &turbinacion[i]);
+        }
+    }
+    
+    // 3 - Nuclear
+    for (i = 0; i < 7; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &nuclear[i]);
+        }
+    }
+    
+    // 4 - Carbon
+    for (i = 0; i < 8; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &carbon[i]);
+        }
+    }
+    
+    // 5 - Motor diesel
+    for (i = 0; i < 9; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &motor[i]);
+        }
+    }
+    
+    // 6 - Turbina gas
+    for (i = 0; i < 10; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &turbinag[i]);
+        }
+    }
+
+    // 7 - Turbina vapor
+    for (i = 0; i < 11; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &turbinav[i]);
+        }
+    }
+    
+    // 8 - Ciclo combinado
+    for (i = 0; i < 12; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &ciclo[i]);
+        }
+    }
+    
+    // 9 - Hidroelectrica
+    for (i = 0; i < 13; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &hidroelectrica[i]);
+        }
+    }
+    
+    // 10 - Eolica
+    for (i = 0; i < 14; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &eolica[i]);
+        }
+    }
+    
+    // 11 - Solarfotovoltaica
+    for (i = 0; i < 14; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &solarfoto[i]);
+        }
+    }
+    
+    // 12 - Solar termica
+    for (i = 0; i < 15; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &solarter[i]);
+        }
+    }
+
+    // Otras renovables
+    for (i = 0; i < 16; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &renovables[i]);
+        }
+    }
+    
+    // Cogeneracion
+    for (i = 0; i < 17; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &cogeneracion[i]);
+        }
+    }
+    
+    // Resudios no renovales
+    for (i = 0; i < 18; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &residuosnor[i]);
+        }
+    }
+    
+    // Residuos renovables
+    for (i = 0; i < 19; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &residuosr[i]);
+        }
+    }
+    
+    // Generacion total
+    for (i = 0; i < 20; i++) {
+        fscanf(pf, "%*[^\n]%*c");
+    }
+
+    if (fscanf(pf, "%*[^,],") == 0) {
+        for (i = 0; i < 24; i++) {
+            fscanf(pf, "%f,", &generaciontotal[i]);
+        }
+    }
+
+    fclose(pf);
+    
+    printf ("Elija que dato quiere consultar:\n");
+    printf ("(1) Hidraulica\n(2) Turbinacion bombeo\n(3) Nuclear\n(4) Carbon\n(5) Motores diesel\n(6) Turbina de gas\n");
+	printf ("(7) Turbina de vapor\n(8) Ciclo combinado\n(9) Hidroeolica\n(10) Eolica\n(11) Solar fotovoltaica\n(12) Solar termica\n");
+	printf ("(13) Otras renovables\n(14) Cogeneracion\n(15) Residuos no renovables\n(16) Residuos renovables\n(17) Generacion total\n");
+	scanf ("%i", &dato);
+	
+	switch (dato) {
+		case 1:
+			buscar_datos (hidraulica);
+			break;
+		case 2:
+			buscar_datos (turbinacion);
+			break;
+		case 3:
+			buscar_datos (nuclear);
+			break;
+		case 4:
+			buscar_datos(carbon);
+			break;
+		case 5:
+			buscar_datos (motor);
+			break;
+		case 6:
+			buscar_datos (turbinag);
+			break;
+		case 7:
+			buscar_datos (turbinav);
+			break;
+		case 8:
+			buscar_datos (ciclo);
+			break;
+		case 9:
+			buscar_datos (hidroelectrica);
+			break;
+		case 10:
+			buscar_datos (eolica);
+			break;
+		case 11:
+			buscar_datos (solarfoto);
+			break;
+		case 12:
+			buscar_datos (solarter);
+			break;
+		case 13:
+			buscar_datos (renovables);
+			break;
+		case 14:
+			buscar_datos (cogeneracion);
+			break;
+		case 15:
+			buscar_datos (residuosnor);
+			break;
+		case 16:
+			buscar_datos (residuosr);
+			break;
+		case 17:
+			buscar_datos (generaciontotal);
+			break;														
+	}
+	return 0;
+}
+
+void buscar_datos (float v[24]) {
+	
+	int fecha, a, m, m1, i;
+	
+	printf ("Fecha del dato que quiere consultar:\nAno:\n2021\n2022\n");
+	printf ("Si quiere ver los datos de los dos anos escriba 2122\n");
+	scanf ("%i", &fecha);
+	
+	switch (fecha) {
+		case 2021:
+			a = 1;
+			break;
+		case 2022:
+			a = 2;
+			break;
+		case 2122:
+			a = 3;
+			break;
+	}
+	printf ("Mes:\n");
+	printf ("Si quiere ver los datos de todos los meses pulse (24)\n");
+	scanf ("%i", &m);
+	m = m - 1;
+			
+	if (a == 1) {
+		if (m != 23) {
+			printf ("%f\n", v [m]);
+		}
+		else if (m == 23) {
+			for (i = 0; i < 11; i++) {
+				printf ("%f  ", v [i]);
+			}
+		}
+	}
+	if (a == 2) {
+		if (m != 23) {
+			m = m + 12;
+			printf ("%f \n", v [m]);
+		}
+		else if (m == 23) {
+			for (i = 12; i < m; i++) {
+				printf ("%f  ", v [i]);
+			}	
+		}
+	}
+	if (a == 3) {
+		if (m != 23) {
+			m1 = m + 12;
+			printf ("%f\n", v [m]);
+			printf ("%f\n", v [m1]);
+		}
+		else if (m == 23) {
+			for (i = 0; i < m; i++) {
+				printf ("%f  ", v [i]);
+			}
+		}
+	}
+}
+
