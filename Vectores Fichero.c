d713608be405b0c0ebf62eac1a5506a8e8d50367
arnoldcampoverde <arnold.campoverde@alumnos.upm.es>
2023-05-28 19:04:45 +0200

    Update Vectores Fichero.c

--- Vectores Fichero.c
+++ Vectores Fichero.c
@@ -4,8 +4,6 @@ void vectores_energia (char* fichero, float v[24], int x);
 
 int main () {
 	
-	char fichero1[] = "c:/Users/Fernanda/OneDrive/Desktop/Trabajo/generacion_por_tecnologias_21_22_puntos_simplificado.csv";
-	
 	void vectores_energia (char* fichero, float v[24], int x);
 	
 	return 0;
@@ -13,7 +11,7 @@ int main () {
 
 void vectores_energia (char* fichero, float v[24], int x) {
 	
-    FILE* fp = fopen(fichero, "r");
+    FILE* fp = fopen (fichero, "r");
     if (fp == NULL) {
         printf ("No se ha podido abrir el fichero.\n");
         return;
@@ -24,7 +22,7 @@ void vectores_energia (char* fichero, float v[24], int x) {
         fscanf (fp, "%*[^\n]%*c");
     }
 
-    if (fscanf(fp, "%*[^,],") == 0) {
+    if (fscanf (fp, "%*[^,],") == 0) {
         for (i = 0; i < 24; i++) {
             fscanf (fp, "%f,", &v[i]);
         }
