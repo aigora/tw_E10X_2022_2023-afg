f11fe9f6f921f5983575285861069d18f3ee5c1a
FernandaBasagoiti <102177781+FernandaBasagoiti@users.noreply.github.com>
2023-05-29 21:52:59 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -11,40 +11,43 @@ int main() {
         
         printf("A continuación, dispone del menu de opciones que ofrecemos:\n\n");
         
-        printf("1. Información general \n");
-        printf("2. Datos del gasto energético\n");
-        printf("3. Cálculos estadísticos\n");
-        printf("4. Comparación con otros paises\n");
-        printf("5. \n");
-        printf("6. Salir\n");
-        printf("Ingrese una opción: \n\n");
+        printf("1. Informacion general \n");
+        printf("2. Datos del gasto energetico\n");
+        printf("3. Cálculos estadisticos\n");
+        printf("4. Busqueda de datos\n");
+        printf("5. Ordenar datos\n");
+        printf("6. Diagrama de barras\n");
+        printf("7. Salir\n");
+        printf("Ingrese una opcion: \n\n");
         scanf("%d", &opcion);
 
-        // Realizar acciones según la opción seleccionada
+        // Realizar acciones según la opcion seleccionada
         switch (opcion) {
             case 1:
-                printf("Has seleccionado la opción 1: Información general\n\n");
-                
-                printf("Durante los años 2021 y 2022, España combinó fuentes convencionales y renovables en su generación de energía eléctrica. Destacaron la energía eólica y solar como principales fuentes renovables. Se instalaron nuevos parques eólicos y plantas solares, aprovechando los recursos naturales del país. Aunque las fuentes convencionales como la energía nuclear y los combustibles fósiles tuvieron un papel importante, se observó una disminución gradual en la generación de electricidad a partir del carbón. Se realizaron avances en infraestructuras de transporte y almacenamiento de energía, implementando tecnologías de almacenamiento para una gestión eficiente de la energía renovable. En resumen, España avanzó hacia una generación más sostenible, combinando fuentes renovables y convencionales para satisfacer la demanda eléctrica y reducir las emisiones de gases de efecto invernadero..\n\n");
-                
+                printf("Has seleccionado la opcion 1: Informacion general\n\n");
+                //Informacion general                
                 break;
             case 2:
-                printf("Has seleccionado la opción 2: \n");
-               //gasto energético
+                printf("Has seleccionado la opcion 2: Gasto energetico\n");
+               //gasto energetico
                 break;
             case 3:
-                printf("Has seleccionado la opción 3: \n");
-                //cálculos estadísticos
+                printf("Has seleccionado la opcion 3: calculos estadisticos\n");
+                //cálculos estadisticos
                 break;
             case 4:
-                printf("Has seleccionado la opción 4: \n");
-                //comparación con otros paises
+                printf("Has seleccionado la opcion 4: Busqueda de datos \n");
+                //busqueda de datos
                 break;
             case 5:
-                printf("Has seleccionado la opción 5: \n");
-                
+                printf("Has seleccionado la opcion 5: Ordenar datos \n");
+                //ordenar datos
                 break;
+            
             case 6:
+                printf ("Has seleccionado la opcion 6: Diagrama de barras \n);
+                //Diagrama de barras
+            case 7:
                 printf("Saliendo del programa...\n");
                 break;
             default:
@@ -53,7 +56,7 @@ int main() {
         }
 
         printf("\n");
-    } while (opcion != 6);
+    } while (opcion != 7);
 
     return 0;
 }
