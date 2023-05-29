71bb697770293d878f69598e7810892785873186
arnoldcampoverde <arnold.campoverde@alumnos.upm.es>
2023-05-29 11:27:19 +0200

    Update Vectores Fichero.c

--- Vectores Fichero.c
+++ Vectores Fichero.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 
-void vectores_energia (char* fichero, float v[24], int x);
+void vectores_energia (char* fichero, float v[24], int x, int k);
 
 int main () {
 	
-	void vectores_energia (char* fichero, float v[24], int x);
+	void vectores_energia (char* fichero, float v[24], int x, int k);
 	
 	return 0;
 }
 
-void vectores_energia (char* fichero, float v[24], int x) {
-	
-    FILE* fp = fopen (fichero, "r");
+void vectores_energia(char* fichero, float v[24], int x, int k) {
+    FILE* fp = fopen(fichero, "r");
     if (fp == NULL) {
-        printf ("No se ha podido abrir el fichero.\n");
+        printf("No se ha podido abrir el fichero.\n\n");
         return;
     }
-    
-    int i;
-    for (i = 0; i < x; i++) {
-        fscanf (fp, "%*[^\n]%*c");
-    }
 
-    if (fscanf (fp, "%*[^,],") == 0) {
-        for (i = 0; i < 24; i++) {
-            fscanf (fp, "%f,", &v[i]);
+    int i;
+    char c;
+
+    for (i = 0; i < 5+x; i++) {
+        while (fscanf(fp, "%c", &c) != EOF && c != '\n') {
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == 'á' || c == 'é' || c == 'í' || c == 'ó' || c == 'ú') {
+                c = ',';
+            }
         }
     }
 
-    fclose (fp);
+    for (i = 0; i < k; i++) {
+        fscanf(fp, "%c", &c);
+    }
     
+    for (i = 0; i < 24; i++) {
+        fscanf(fp, ",%f", &v[i]);
+    }
+
+    fclose(fp);
 }
